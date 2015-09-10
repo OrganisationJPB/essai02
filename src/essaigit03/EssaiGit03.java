eb39c3de89fbfa90705d1b2005679969dd1781a0
jp <jp@PC-JP>
2015-09-10 12:12:54 +0200

    commit 5

--- src/essaigit03/EssaiGit03.java
+++ src/essaigit03/EssaiGit03.java
@@ -18,11 +18,11 @@ public class EssaiGit03 {
         int i;
         i = 10;
         System.out.println("la valeur de i est : " + i);
-        
-        int j;
-                j = 10;
-        System.out.println("la valeur de j est : " + j);
-        
+
+        int k;
+        k = 10;
+        System.out.println("la valeur de j est : " + k);
+
     }
-    
+
 }
